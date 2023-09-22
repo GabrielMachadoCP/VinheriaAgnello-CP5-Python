49fb47932bce13463c0773560cf1e9e9d32a47b6
Gabriel Machado Carrara Pimentel <126582480+GabrielMachadoCP@users.noreply.github.com>
2023-09-22 17:11:47 -0300

    Update sth-comet.py

--- sth-comet.py
+++ sth-comet.py
@@ -1,41 +1,65 @@
-# Código para obter as leituras de luminosidade por minutos em um intervalo de tempo
 import requests
+
 import matplotlib.pyplot as plt
+
 from datetime import datetime
 
-# Fazer a solicitação à API
-url = "http://46.17.108.113:8666/STH/v1/contextEntities/type/Lamp/id/urn:ngsi-ld:Lamp:001/attributes/luminosity?aggrMethod=max&aggrPeriod=minute&dateFrom=2023-09-18T22:30:21.237&dateTo=2023-09-18T22:45:21.237"
+ 
+
+url="http://46.17.108.113:8666/STH/v1/contextEntities/type/Lamp/id/urn:ngsi-ld:Lamp:001/attributes/luminosity?hLimit=100&hOffset=1&dateFrom=2023-09-22T10:00:00.000&dateTo=2023-09-22T10:15:00.000"
+
+ 
+
+payload = {}
 
 headers = {
-    'fiware-service': 'smart',
-    'fiware-servicepath': '/'
+
+  'fiware-service': 'smart',
+
+  'fiware-servicepath': '/'
+
 }
+
+ 
 
 response = requests.get(url, headers=headers)
 
+ 
+
 if response.status_code == 200:
+
     data = response.json()
-    values = data['contextResponses'][0]['contextElement']['attributes'][0]['values'][0]['points']
-    origin_value = data["contextResponses"][0]["contextElement"]["attributes"][0]["values"][0]["_id"]["origin"]
-    print(f"Origin: {origin_value}")
 
-    # Extrair os valores de luminosidade e tempos
-    luminosidade = [point['max'] for point in values]
-    tempos = [point['offset'] for point in values]
+    values = [entry['attrValue'] for entry in data['contextResponses'][0]['contextElement']['attributes'][0]['values']]
 
-    # Plotar o gráfico
-    plt.figure(figsize=(12, 6))
-    
-    # Converter os tempos para o formato desejado (eixo x do gráfico)
-    tempos_formatados = [f"2023-09-18T22:{offset:02d}:00.000Z" for offset in tempos]
+    timestamps = [datetime.strptime(entry['recvTime'], '%Y-%m-%dT%H:%M:%S.%fZ') for entry in data['contextResponses'][0]['contextElement']['attributes'][0]['values']]
 
-    plt.plot(tempos_formatados, luminosidade, marker='*', linestyle='-', color='g')
-    plt.title('Gráfico de Luminosidade em Função do Tempo')
-    plt.xlabel('Tempo')
-    plt.ylabel('Luminosidade (Max)')
+   
+
+    # Agora, vamos criar o gráfico.
+
+    plt.figure(figsize=(10, 6))
+
+    plt.plot(timestamps, values, marker='o', linestyle='-')
+
+    plt.xlabel('Timestamp')
+
+    plt.ylabel('Luminosity')
+
+    plt.title('Luminosity Over Time')
+
     plt.grid(True)
+
+    plt.xticks(rotation=45)
+
     plt.tight_layout()
-    plt.xticks(rotation=45)  # Rotacionar rótulos do eixo x para melhor visualização
+
+   
+
+    # Exibir o gráfico
+
     plt.show()
+
 else:
-    print(f"Erro ao obter dados: {response.status_code}")
+
+    print(f"Failed to fetch data. Status code: {response.status_code}")
